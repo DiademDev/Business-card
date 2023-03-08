e445e458c71dc1ab47d71014d78e8e7c40d3c0f2
DiademDev <118876421+DiademDev@users.noreply.github.com>
2023-03-08 14:53:25 +1300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
-const THREE = window.MINDAR.IMAGE.THREE
+const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    const start = async () => {
+
     const mindarthree = new window.MINDAR.IMAGE.MindARThree ({
-        container: document.body
-    })
-})
+        container: document.body,
+        imageTargetSrc: '../assets/Bus-card.mind'
+    });
+
+    const {renderer, scene, camera} = mindarThree;
+
+    const geometry = new THREE.BoxGeometry(1,1,1);
+    const material = new THREE.MeshBasicMaterial({color: 0x0000ff});
+    const cube = new THREE.Mesh(goemetry, material);
+
+    const anchor = mindarThree.addAnchor(0);
+    anchor.group.add(cube);
+
+    await mindarThree. start();
+
+    renderer.setAnimationLoop(() => {
+        renderer.render(scene, camera);
+    });
+    }
+    start();
+});
