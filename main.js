0ee5abf7b9bef2f991b07f24a63f274dd11e9403
DiademDev <118876421+DiademDev@users.noreply.github.com>
2023-03-08 14:41:07 +1300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const THREE = window.MINDAR.IMAGE.THREE
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const mindarthree = new window.MINDAR.IMAGE.MindARThree ({
+        container: document.body
+    })
+})
